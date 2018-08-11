a5a05ac75902cd1b26ceda6eb55ab1c47f925729
YutoMori <b1016206@fun.ac.jp>
2018-08-11 16:36:32 +0900

    Add WaterHandler and BoiledHandler

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const Alexa = require('ask-sdk');
  
 const SKILL_NAME = 'ゆで卵タイマー';
 const which_heater = 'IHを使用しますか？ガスを使用しますか？';
+const ask_heater_message = 'IHかガスと言ってください。';
+
 const which_water = '水から作りますか？お湯から作りますか？';
+const ask_water_message = '水かお湯と言ってください。'
 
 const Again_Message = 'もう一度言ってください';
 
@@ -26,6 +29,7 @@ exports.handler = async function (event, context) {
             LaunchRequestHandler,
             CookingHeaterHandler,
             WaterHandler,
+            BoiledHandle,
             StopIntentHandler,
             SessionEndedRequestHandler,
             ErrorHandler
@@ -33,7 +37,7 @@ exports.handler = async function (event, context) {
         .create();
     }
     return skill.invoke(event);
-}
+};
 
 
 const LaunchRequestHandler = {
@@ -45,11 +49,10 @@ const LaunchRequestHandler = {
 
     handle(handlerInput) {
         const launch_message = '一緒に美味しいゆで卵を作りましょう。';
-        const ask_message = 'IHかガスと言ってください。';
 
         return handlerInput.responseBuilder
             .speak(launch_message + which_heater)
-            .reprompt(ask_message)
+            .reprompt(ask_heater_message)
             .getResponse();
     }
 };
@@ -64,13 +67,16 @@ const CookingHeaterHandler = {
     },
     handle(handlerInput) {
         const ask_heater = handlerInput.requestEnvelope.request.intent.slots.Heater.value;
-        handlerInput.attributesManager.setSessionAttributes({'att_heater': ask_heater});
         const heater_message = ask_heater + 'ですね。';
 
         if(heater_list.indexOf(ask_heater) > -1){
+
+            // heaterの種類を受け取ったらSessionAttributesに保存する
+            handlerInput.attributesManager.setSessionAttributes({'att_heater': ask_heater});
+
             return handlerInput.responseBuilder
                 .speak(heater_message + which_water)
-                .reprompt()
+                .reprompt(ask_heater_message)
                 .getResponse();
         } else {
             return handlerInput.responseBuilder
@@ -93,9 +99,19 @@ const WaterHandler = {
         const water_message = ask_water + 'ですね。';
         const asked_heater = handlerInput.attributesManager.getSessionAttributes().att_heater;
 
-        if(liquid_list.indexOf(ask_water) > -1){
+        // heater情報が未設定
+        if(!asked_heater){
             return handlerInput.responseBuilder
-                .speak(water_message+asked_heater)
+                .speak('クッキングヒーターが未設定です。' + which_heater)
+                .reprompt(ask_heater_message)
+                .getResponse();
+        }
+
+        if(liquid_list.indexOf(ask_water) > -1){
+            handlerInput.attributesManager.setSessionAttributes({'att_water': ask_water});
+            return handlerInput.responseBuilder
+                .speak(water_message + '茹で加減はどうしますか？')
+                .reprompt(ask_water_message)
                 .getResponse();
         } else {
             return handlerInput.responseBuilder
@@ -106,7 +122,43 @@ const WaterHandler = {
 
 
     }
-}
+};
+
+const BoiledHandle = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'BoiledIntent';
+    },
+    handle(handlerInput) {
+        const ask_boiled = handlerInput.requestEnvelope.request.intent.slots.Boiled.value;
+        const boiled_message = ask_boiled + 'ですね。';
+
+        const asked_heater = handlerInput.attributesManager.getSessionAttributes().att_heater;
+        // heater情報が未設定(TODO: DynamoDBに入れる)
+        /*
+        if(!asked_heater){
+            return handlerInput.responseBuilder
+                .speak('クッキングヒーターが未設定です。' + which_heater)
+                .reprompt(ask_heater_message)
+                .getResponse();
+        }
+        */
+        const asked_water = handlerInput.attributesManager.getSessionAttributes().att_water;
+        // water情報が未設定
+        if(!asked_water){
+            return handlerInput.responseBuilder
+                .speak('何から加熱するかが決まっていません。' + which_water)
+                .reprompt(ask_water_message)
+                .getResponse();
+        }
+        // TODO 
+        return handlerInput.responseBuilder
+            .speak(boiled_message + asked_water + 'から加熱するには12分かかります。「alexa、12分のタイマーをかけて。」と言ってください。')
+            .getResponse();
+    }
+};
 
 const StopIntentHandler = {
 
@@ -145,4 +197,4 @@ const ErrorHandler = {
             .speak('うまくいきませんでした、ごめんなさい。')
             .getResponse();
     }
-}
+};
